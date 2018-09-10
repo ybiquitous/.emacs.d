69216c556b1360c066d5381a6e098fbd5e884683
Masafumi Koba <ybiquitous@gmail.com>
2018-09-11 08:57:43 +0900

    Improve command showing current key bindings
    - Rename function from `my/describe-favorite-key-bindings` to `my/cheatsheet`
    - Quote functions

--- inits/init-key-bindings.el
+++ inits/init-key-bindings.el
@@ -1,29 +1,31 @@
-(defun my/describe-favorite-key-bindings ()
+(defun my/cheatsheet ()
   "Display favorite key bindings."
   (interactive)
-  (with-output-to-temp-buffer "*Favorite Key Bindings*"
-    (princ "-*- Favorite Key Bindings -*-
+  (with-output-to-temp-buffer "*Cheatsheet*"
+    (princ "-*- Cheatsheet -*-
 
 Key        Function
 -------    -------------------------------------------
-C-? b      describe-bindings
-C-? f      describe-function
-C-? k      describe-key
-C-q        quoted-insert (e.g. `C-q TAB')
-C-x C-+    text-scale-adjust (increase)
-C-x C--    text-scale-adjust (decrease)
-C-x C-0    text-scale-adjust (reset)
-C-x SPC    rectangle-mark-mode
-C-c C-g    vc-git-grep
-C-x v =    vc-diff (git diff)
-C-x v g    vc-annotate (git blame)
-C-x v l    vc-print-log (git log)
-M-!        shell-command
-M-&        async-shell-command
-C-u M-!    shell-command (insert to buffer)
-C-u M-|    shell-command-on-region (insert to buffer)
-C-M-s      isearch-forward-regexp
-M-s w      isearch-forward-word")))
+C-c k b    `describe-personal-keybindings'
+C-? b      `describe-bindings'
+C-? f      `describe-function'
+C-? k      `describe-key'
+C-q        `quoted-insert' (e.g. `C-q TAB')
+C-x C-+    `text-scale-adjust' (↑)
+C-x C--    `text-scale-adjust' (↓)
+C-x C-0    `text-scale-adjust' (reset)
+C-x SPC    `rectangle-mark-mode'
+C-c C-g    `vc-git-grep'
+C-x v =    `vc-diff' (git diff)
+C-x v g    `vc-annotate' (git blame)
+C-x v l    `vc-print-log' (git log)
+M-!        `shell-command'
+M-&        `async-shell-command'
+C-u M-!    `shell-command' (insert to buffer)
+C-u M-|    `shell-command-on-region' (insert to buffer)
+C-M-s      `isearch-forward-regexp'
+M-s w      `isearch-forward-word'
+")))
 
 ;; use-package installs bind-key automatically
 (require 'bind-key)
@@ -36,7 +38,6 @@ M-s w      isearch-forward-word")))
   ("C-c v"   . browse-url-at-point)
   ("C-c l"   . toggle-truncate-lines)
   ("C-c a r" . align-regexp)
-  ("C-c k b" . describe-personal-keybindings)
-  ("C-c f k" . my/describe-favorite-key-bindings))
+  ("C-c k b" . describe-personal-keybindings))
 
 (provide 'init-key-bindings)
