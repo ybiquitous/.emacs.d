b644c66eb9f10c4fb1d205084aea3f39642140e5
Masafumi Koba <ybiquitous@gmail.com>
2018-09-11 08:44:01 +0900

    Customize `npm` project settings in `projectile-mode`

--- inits/init-projectile.el
+++ inits/init-projectile.el
@@ -2,6 +2,11 @@
   :delight " PJ"
   :config
   (projectile-mode)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-register-project-type
+    'npm '("package.json")
+    :compile "npm install"
+    :test "npm test"
+    :test-suffix ".test"))
 
 (provide 'init-projectile)
