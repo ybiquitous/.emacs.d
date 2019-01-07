3f7c07e5e383cc2e60b1acd6ca9fbf0a4b69ae75
Masafumi Koba <ybiquitous@gmail.com>
2019-01-07 23:47:59 +0900

    Refactor tide settings

--- inits/init-typescript.el
+++ inits/init-typescript.el
@@ -2,21 +2,18 @@
   :delight "TS")
 
 (use-package tide
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+          (typescript-mode . tide-hl-identifier-mode))
   :config
-  (defun my/setup-tide-mode ()
-    (tide-setup)
-    (eldoc-mode +1)
-    (tide-hl-identifier-mode +1)
-    (company-mode +1))
-  (add-hook 'typescript-mode-hook #'my/setup-tide-mode)
-
   ;; TSX
   (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
   (flycheck-add-mode 'typescript-tslint 'web-mode)
   (defun my/enable-tsx-on-web-mode ()
     (if (string-equal "tsx" (file-name-extension buffer-file-name))
-      (my/setup-tide-mode)
-      (setq-local flycheck-disabled-checkers '(typescript-tslint tsx-tide jsx-tide))))
+      (progn
+        (tide-setup)
+        (tide-hl-identifier-mode))))
   (add-hook 'web-mode-hook #'my/enable-tsx-on-web-mode))
 
 (provide 'init-typescript)
