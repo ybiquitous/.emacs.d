dfbb1fe25707eb632ba79ed14ff3102fcb248702
Masafumi Koba <ybiquitous@gmail.com>
2019-01-21 10:52:54 +0900

    Open dictionary on Mac

--- inits/init-mac.el
+++ inits/init-mac.el
@@ -3,4 +3,10 @@
 (setq ns-alternate-modifier (quote super))
 (setq ns-command-modifier (quote meta))
 
+;; Launch dictionary
+(defun my/mac-dictionary ()
+  (interactive)
+  (browse-url (concat "dict://" (current-word))))
+(bind-key "C-c D" #'my/mac-dictionary)
+
 (provide 'init-mac)
