32a091ea5cf2d6f57c86c52bb06f032984bf0596
Masafumi Koba <ybiquitous@gmail.com>
2019-01-25 14:48:34 +0900

    Fix TSX settings

--- inits/init-typescript.el
+++ inits/init-typescript.el
@@ -4,16 +4,16 @@
 (use-package tide
   :after (typescript-mode company flycheck)
   :hook ((typescript-mode . tide-setup)
-          (typescript-mode . tide-hl-identifier-mode))
+         (typescript-mode . tide-hl-identifier-mode))
   :config
-  ;; TSX
-  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-  (flycheck-add-mode 'typescript-tslint 'web-mode)
   (defun my/enable-tsx-on-web-mode ()
-    (if (string-equal "tsx" (file-name-extension buffer-file-name))
-      (progn
-        (tide-setup)
-        (tide-hl-identifier-mode))))
+    (when (string-equal "tsx" (file-name-extension buffer-file-name))
+      (tide-setup)
+      (tide-hl-identifier-mode)))
   (add-hook 'web-mode-hook #'my/enable-tsx-on-web-mode))
+
+;; Enable web-mode on TSX
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(flycheck-add-mode 'typescript-tslint 'web-mode)
 
 (provide 'init-typescript)
