1db2eb8a6d0c160ac55836a9929bc1aae0e99eb3
Masafumi Koba <ybiquitous@gmail.com>
2019-01-31 09:40:20 +0900

    Increase `fill-column` on `git-commit-mode`

--- inits/init-git.el
+++ inits/init-git.el
@@ -6,7 +6,7 @@
   (add-hook 'git-commit-mode-hook #'flyspell-mode)
   :custom
   (git-commit-summary-max-length 100)
-  (fill-column 100))
+  (fill-column 150))
 
 ;; disable git pager
 (setenv "GIT_PAGER" "")
