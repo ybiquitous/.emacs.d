29c926dfa44613240af60a20a43e230ce6a90d3e
Masafumi Koba <ybiquitous@gmail.com>
2019-02-18 09:53:52 +0900

    Refactor Ruby settings

--- inits/init-ruby.el
+++ inits/init-ruby.el
@@ -1,15 +1,6 @@
 (require 'ruby-mode)
 (setq ruby-insert-encoding-magic-comment nil)
 
-(require 'company)
-
-(use-package robe
-  :config
-  (eval-after-load 'company
-    '(push 'company-robe company-backends)))
-
-(use-package ruby-electric)
-
 (use-package enh-ruby-mode
   :interpreter "ruby"
   :mode ("\\.rb\\'"
@@ -28,10 +19,14 @@
   (enh-ruby-deep-indent-paren nil)
   (enh-ruby-bounce-deep-indent t))
 
-(defun my/ruby-mode-hook ()
-  (robe-mode t)
-  (ruby-electric-mode t))
-(add-hook 'ruby-mode-hook #'my/ruby-mode-hook)
-(add-hook 'enh-ruby-mode-hook #'my/ruby-mode-hook)
+(use-package ruby-electric
+  :hook ((ruby-mode enh-ruby-mode) . ruby-electric-mode))
+
+(require 'company)
+(use-package robe
+  :hook ((ruby-mode enh-ruby-mode) . robe-mode)
+  :config
+  (eval-after-load 'company
+    '(push 'company-robe company-backends)))
 
 (provide 'init-ruby)
