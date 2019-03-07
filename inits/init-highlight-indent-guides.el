66263dc8a76072a4490086f59e52cf59f86e453e
Masafumi Koba <ybiquitous@gmail.com>
2019-03-07 09:56:01 +0900

    Enable `highlight-indent-guides-mode` on JSX files

--- inits/init-highlight-indent-guides.el
+++ inits/init-highlight-indent-guides.el
@@ -1,6 +1,6 @@
 (use-package highlight-indent-guides
-  :after (yaml-mode)
-  :hook ((yaml-mode my-json-mode) . highlight-indent-guides-mode)
+  :after (yaml-mode web-mode rjsx-mode)
+  :hook ((my-json-mode yaml-mode web-mode rjsx-mode) . highlight-indent-guides-mode)
   :custom
   (highlight-indent-guides-method 'character))
 
