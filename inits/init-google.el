696f289470d996f698b47eda65d9ec31816d2d9f
Masafumi Koba <ybiquitous@gmail.com>
2019-05-06 14:13:40 +0900

    Workaround for `google-translate` search failed
    See <https://github.com/atykhonov/google-translate/issues/52#issuecomment-481310626>.

--- inits/init-google.el
+++ inits/init-google.el
@@ -9,4 +9,8 @@
   (google-translate-default-source-language "en")
   (google-translate-default-target-language "ja"))
 
+;; Workaround for search failed. See https://github.com/atykhonov/google-translate/issues/52#issuecomment-481310626
+(with-eval-after-load "google-translate-tk"
+  (defun google-translate--search-tkk () "Search TKK." (list 430675 2721866130)))
+
 (provide 'init-google)
