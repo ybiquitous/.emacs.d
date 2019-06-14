13a98915397e2ad9601e7a6ece1fd555d8d3c1dc
Masafumi Koba <ybiquitous@gmail.com>
2019-06-14 22:03:39 +0900

    Remove HTML processing from `markdown-mode` settings
    The processing will be delegated to the `github-markup-custom` command.

--- inits/init-markdown.el
+++ inits/init-markdown.el
@@ -5,28 +5,6 @@
   :custom
   (markdown-command "github-markup-custom")
   (markdown-command-needs-filename t)
-  (markdown-spaces-after-code-fence 0)
-  (markdown-content-type "application/xhtml+xml")
-  (markdown-css-paths '("https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown.min.css"
-                        "https://cdn.jsdelivr.net/npm/highlightjs/styles/github.min.css"))
-  (markdown-xhtml-header-content "
-<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
-<style>
-body {
-  max-width: 50rem;
-  width: 100%;
-  margin: 3rem auto;
-  padding: 0 0.5rem;
-}
-</style>")
-  (markdown-xhtml-body-epilogue "
-<script src='https://cdn.jsdelivr.net/npm/highlightjs'></script>
-<script>
-  document.body.classList.add('markdown-body');
-  document.querySelectorAll('pre[lang] > code').forEach(code => {
-    code.classList.add(code.parentElement.lang);
-    hljs.highlightBlock(code);
-  });
-</script>"))
+  (markdown-spaces-after-code-fence 0))
 
 (provide 'init-markdown)
