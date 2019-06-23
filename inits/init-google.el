30121cad6668cce69a24b36ad4c26d5e85b01dfb
Masafumi Koba <ybiquitous@gmail.com>
2019-06-24 07:16:06 +0900

    Fix `google-translate` search error
    * Set the new variable `google-translate-backend-method`.
    * Remove unnecessary workaround.
    
    See also:
    
    * https://github.com/atykhonov/google-translate/issues/52#issuecomment-504052903
    * https://github.com/atykhonov/google-translate/pull/91

--- inits/init-google.el
+++ inits/init-google.el
@@ -7,10 +7,7 @@
          ("C-c T" . google-translate-at-point-reverse))
   :custom
   (google-translate-default-source-language "en")
-  (google-translate-default-target-language "ja"))
-
-;; Workaround for search failed. See https://github.com/atykhonov/google-translate/issues/52#issuecomment-481310626
-(with-eval-after-load "google-translate-tk"
-  (defun google-translate--search-tkk () "Search TKK." (list 430675 2721866130)))
+  (google-translate-default-target-language "ja")
+  (google-translate-backend-method 'curl))
 
 (provide 'init-google)
