6107696c73f83d4023e3a4eb0d16813afdda95db
Masafumi Koba <ybiquitous@gmail.com>
2019-10-30 13:02:10 +0900

    Enable hs-minor-mode on enh-ruby-mode

--- inits/init-ruby.el
+++ inits/init-ruby.el
@@ -17,7 +17,12 @@
   :custom
   (enh-ruby-add-encoding-comment-on-save nil)
   (enh-ruby-deep-indent-paren nil)
-  (enh-ruby-bounce-deep-indent t))
+  (enh-ruby-bounce-deep-indent t)
+  :config
+  ;; https://www.emacswiki.org/emacs/HideShow
+  (add-to-list 'hs-special-modes-alist
+    '(ruby-mode (rx (or "def" "do" "{")) (rx (or "end" "}")) "#" ruby-forward-sexp nil))
+  (add-hook 'enh-ruby-mode-hook #'hs-minor-mode))
 
 (use-package ruby-electric
   :hook ((ruby-mode enh-ruby-mode) . ruby-electric-mode))
