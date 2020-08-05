36ee4ad81ec9209046e1a57152adbcba030c9fc2
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-08-05 11:25:32 +0900

    Set hs-special-modes-alist for ruby-mode and enh-ruby-mode

--- inits/init-ruby.el
+++ inits/init-ruby.el
@@ -18,14 +18,27 @@
   :custom
   (enh-ruby-add-encoding-comment-on-save nil)
   (enh-ruby-deep-indent-paren nil)
-  (enh-ruby-bounce-deep-indent t)
-  :config
-  ;; https://www.emacswiki.org/emacs/HideShow
-  (add-to-list 'hs-special-modes-alist
-    '(ruby-mode (rx (or "def" "do" "{")) (rx (or "end" "}")) "#" ruby-forward-sexp nil))
-  (add-hook 'enh-ruby-mode-hook #'hs-minor-mode))
+  (enh-ruby-bounce-deep-indent t))
 
 (use-package ruby-electric
   :hook ((ruby-mode enh-ruby-mode) . ruby-electric-mode))
+
+;; https://www.emacswiki.org/emacs/HideShow
+(add-to-list 'hs-special-modes-alist
+  '(ruby-mode
+     "class\\|module\\|def\\|if\\|unless\\|case\\|while\\|until\\|for\\|begin\\|do"
+     "end"
+     "#"
+     ruby-move-to-block
+     nil))
+(add-to-list 'hs-special-modes-alist
+  '(enh-ruby-mode
+     "class\\|module\\|def\\|if\\|unless\\|case\\|while\\|until\\|for\\|begin\\|do"
+     "end"
+     "#"
+     ruby-move-to-block
+     nil))
+(add-hook 'ruby-mode-hook #'hs-minor-mode)
+(add-hook 'enh-ruby-mode-hook #'hs-minor-mode)
 
 (provide 'init-ruby)
