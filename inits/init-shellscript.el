cb8251a255f81b7270b5d01b94ec9a187e317daa
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-11-06 23:43:30 +0900

    Open `.envrc` with sh-mode

--- inits/init-shellscript.el
+++ inits/init-shellscript.el
@@ -1,4 +1,7 @@
 (require 'sh-script)
 (setq sh-basic-offset tab-width)
 
+;; https://github.com/direnv/direnv
+(add-to-list 'auto-mode-alist '("\\.envrc\\'" . sh-mode))
+
 (provide 'init-shellscript)
