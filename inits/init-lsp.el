36986453e86c5228a0eb88270177e863d283d691
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-01-05 09:51:29 +0900

    Install yasnippet explicitly for lsp-mode

--- inits/init-lsp.el
+++ inits/init-lsp.el
@@ -1,10 +1,12 @@
 (use-package lsp-mode
-  :commands lsp
-  :custom
-  (lsp-enable-snippet nil)
-  (lsp-restart "auto-restart"))
+  :commands lsp)
 
 (use-package lsp-ui
   :commands lsp-ui-mode)
+
+;; NOTE: lsp-mode requires implicitly yasnippet.
+(use-package yasnippet
+  :init
+  (add-hook 'prog-mode-hook #'yas-minor-mode))
 
 (provide 'init-lsp)
