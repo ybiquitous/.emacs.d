0ad333b9e7f44562f8028d3fb5e853fca6eec981
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-01-05 09:50:28 +0900

    Add `LANG` to `exec-path-from-shell-variables`

--- inits/init-exec-path.el
+++ inits/init-exec-path.el
@@ -5,6 +5,7 @@
   (add-to-list 'exec-path-from-shell-variables "HISTSIZE")
   (add-to-list 'exec-path-from-shell-variables "EDITOR")
   (add-to-list 'exec-path-from-shell-variables "VISUAL")
+  (add-to-list 'exec-path-from-shell-variables "LANG")
   (exec-path-from-shell-initialize))
 
 (provide 'init-exec-path)
