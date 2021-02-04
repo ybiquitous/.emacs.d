1a0b26e10fd1d1586a832d51d9d9c624e02fadc2
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-02-04 09:41:06 +0900

    Fix and customize markdown-mode

--- inits/init-markdown.el
+++ inits/init-markdown.el
@@ -2,6 +2,11 @@
   :commands (markdown-mode gfm-mode)
   :mode (("\\.md\\'" . gfm-mode)
          ("\\.markdown\\'" . gfm-mode))
+  :init
+  (defun my/customize-markdown-mode ()
+    (setq-local whitespace-line-column 1000)
+    (visual-line-mode))
+  (add-hook 'markdown-mode-hook #'my/customize-markdown-mode)
   :custom
   (markdown-command "commonmarker --extension=autolink,strikethrough,table,tagfilter,tasklist")
   (markdown-css-paths '("https://cdn.jsdelivr.net/npm/github-markdown-css"
@@ -39,7 +44,6 @@
   (markdown-xhtml-body-epilogue "</div>")
   (markdown-command-needs-filename t)
   (markdown-spaces-after-code-fence 0)
-  (markdown-fontify-code-blocks-natively t)
-  (whitespace-line-column 1000))
+  (markdown-fontify-code-blocks-natively t))
 
 (provide 'init-markdown)
