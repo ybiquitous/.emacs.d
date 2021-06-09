16094ee5b32fec2dcba6db6a1cb5048f0c96357f
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-06-09 22:43:14 +0900

    Set emoji font on macOS

--- inits/init-mac.el
+++ inits/init-mac.el
@@ -9,4 +9,9 @@
   (browse-url (concat "dict://" (current-word))))
 (bind-key "C-c D" #'my/mac-dictionary)
 
+;; Emoji
+;; https://github.crookster.org/emacs27-from-homebrew-on-macos-with-emoji/
+(if (version< "27.0" emacs-version)
+  (set-fontset-font "fontset-default" 'unicode "Apple Color Emoji" nil 'prepend))
+
 (provide 'init-mac)
