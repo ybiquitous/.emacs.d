855c94d589ec8eaa9de6baf6e4526409f3ff01d1
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-08-20 12:18:02 +0900

    Add `dired-sidebar` package

--- inits/init-dired.el
+++ inits/init-dired.el
@@ -2,4 +2,8 @@
 (setq wdired-allow-to-change-permissions t)
 (define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)
 
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :commands (dired-sidebar-toggle-sidebar))
+
 (provide 'init-dired)
