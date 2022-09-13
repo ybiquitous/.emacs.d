c88e19692a611284bc60e52b6fcd2a49c1497679
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-09-13 10:10:56 +0900

    Remove Flycheck settings from typescript-mode
    Usually using lsp-mode instead of Flycheck.

--- inits/init-typescript.el
+++ inits/init-typescript.el
@@ -1,7 +1,4 @@
 (use-package typescript-mode
-  :after (flycheck)
-  :delight "TS"
-  :config
-  (flycheck-add-mode 'javascript-eslint 'typescript-mode))
+  :delight "TS")
 
 (provide 'init-typescript)
