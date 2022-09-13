181d34c3413ee02b01826e6ad8de07e108931456
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-09-13 14:11:57 +0900

    Fix hooks for `add-node-modules-path`

--- inits/init-javascript.el
+++ inits/init-javascript.el
@@ -13,14 +13,14 @@
 
 (use-package add-node-modules-path
   :custom
-  (add-node-modules-path-debug t)
-  :config
-  (eval-after-load 'js-mode '(add-hook 'js-mode-hook #'add-node-modules-path))
-  (eval-after-load 'json-mode '(add-hook 'json-mode-hook #'add-node-modules-path))
-  (eval-after-load 'typescript-mode '(add-hook 'typescript-mode-hook #'add-node-modules-path))
-  (eval-after-load 'css-mode '(add-hook 'css-mode-hook #'add-node-modules-path))
-  (eval-after-load 'web-mode '(add-hook 'web-mode-hook #'add-node-modules-path))
-  (eval-after-load 'markdown-mode '(add-hook 'markdown-mode-hook #'add-node-modules-path))
-  (eval-after-load 'yaml-mode '(add-hook 'yaml-mode-hook #'add-node-modules-path)))
+  (add-node-modules-path-debug t))
+
+(add-hook 'js-mode-hook #'add-node-modules-path)
+(eval-after-load 'json-mode '(add-hook 'json-mode-hook #'add-node-modules-path))
+(eval-after-load 'typescript-mode '(add-hook 'typescript-mode-hook #'add-node-modules-path))
+(eval-after-load 'css-mode '(add-hook 'css-mode-hook #'add-node-modules-path))
+(eval-after-load 'web-mode '(add-hook 'web-mode-hook #'add-node-modules-path))
+(eval-after-load 'markdown-mode '(add-hook 'markdown-mode-hook #'add-node-modules-path))
+(eval-after-load 'yaml-mode '(add-hook 'yaml-mode-hook #'add-node-modules-path))
 
 (provide 'init-javascript)
