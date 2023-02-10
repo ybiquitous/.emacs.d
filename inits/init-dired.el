83d6676929b8a818b519ed6d224bf5bff11de4a6
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-02-10 13:49:12 +0900

    Customize `dired-sidebar-mode`

--- inits/init-dired.el
+++ inits/init-dired.el
@@ -4,6 +4,13 @@
 
 (use-package dired-sidebar
   :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
-  :commands (dired-sidebar-toggle-sidebar))
+  :commands (dired-sidebar-toggle-sidebar)
+  :custom
+  (dired-sidebar-width 25)
+  (dired-sidebar-use-custom-font t)
+  :config
+  (defun my/disable-linum-mode-on-dired-sidebar-mode ()
+    (linum-mode -1))
+  (add-hook 'dired-sidebar-mode-hook #'my/disable-linum-mode-on-dired-sidebar-mode))
 
 (provide 'init-dired)
