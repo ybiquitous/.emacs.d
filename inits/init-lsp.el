0b79d93050d2c935f618a5e66073c05f3307092e
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-06-28 23:11:12 +0900

    Remove unneeded `yasnippet` as an implicitly dependency of `lsp-mode`

--- inits/init-lsp.el
+++ inits/init-lsp.el
@@ -6,9 +6,4 @@
 (use-package lsp-ui
   :commands lsp-ui-mode)
 
-;; NOTE: lsp-mode requires implicitly yasnippet.
-(use-package yasnippet
-  :init
-  (add-hook 'prog-mode-hook #'yas-minor-mode))
-
 (provide 'init-lsp)
