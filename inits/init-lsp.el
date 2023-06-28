b8e6d60143ae28978940609d01090a3810a86803
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-06-29 00:09:46 +0900

    Add `lsp-mode` client for RuboCop

--- inits/init-lsp.el
+++ inits/init-lsp.el
@@ -3,4 +3,12 @@
   :custom
   (lsp-disabled-clients '(deno-ls)))
 
+;; https://emacs-lsp.github.io/lsp-mode/page/adding-new-language/
+(with-eval-after-load 'lsp-mode
+  (lsp-register-client
+    (make-lsp-client :new-connection (lsp-stdio-connection '("bundle" "exec" "rubocop" "--lsp"))
+                     :activation-fn (lsp-activate-on "ruby")
+                     :add-on? t
+                     :server-id 'rubocop-ls)))
+
 (provide 'init-lsp)
