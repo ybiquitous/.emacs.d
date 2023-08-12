77c8340f9bd1ca24dfe9e5beaac030ae49f9905c
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-08-12 22:59:18 +0900

    Refactor `init-javascript` settings

--- inits/init-javascript.el
+++ inits/init-javascript.el
@@ -1,30 +1,18 @@
-(require 'js)
-(setq js-indent-level tab-width)
-(setq js-switch-indent-offset tab-width)
-
-;; Node.js supports `.mjs' and `.cjs': https://nodejs.org/api/packages.html
-(add-to-list 'auto-mode-alist '("\\.[cm]js\\'" . js-mode))
-
-;; Override 'js-find-symbol
-(define-key js-mode-map (kbd "M-.") #'xref-find-definitions)
-
-;; https://www.emacswiki.org/emacs/HideShow
-(add-hook 'js-mode-hook #'hs-minor-mode)
+(use-package js
+  :custom
+  (js-indent-level tab-width)
+  (js-switch-indent-offset tab-width)
+  :init
+  ;; Node.js supports `.mjs' and `.cjs': https://nodejs.org/api/packages.html
+  (add-to-list 'auto-mode-alist '("\\.[cm]js\\'" . js-mode)))
 
 (use-package add-node-modules-path
   :custom
   (add-node-modules-path-debug t)
   ;; HACK: https://github.com/codesuki/add-node-modules-path/issues/23
   (add-node-modules-path-command "echo \"$(npm root)/.bin\"")
+  :hook ((prog-mode markdown-mode yaml-ts-mode) . add-node-modules-path)
   :init
   (add-to-list 'warning-suppress-log-types '(add-node-modules-path)))
-
-(add-hook 'js-mode-hook #'add-node-modules-path)
-(eval-after-load 'json-mode '(add-hook 'json-mode-hook #'add-node-modules-path))
-(eval-after-load 'typescript-mode '(add-hook 'typescript-mode-hook #'add-node-modules-path))
-(eval-after-load 'css-mode '(add-hook 'css-mode-hook #'add-node-modules-path))
-(eval-after-load 'web-mode '(add-hook 'web-mode-hook #'add-node-modules-path))
-(eval-after-load 'markdown-mode '(add-hook 'markdown-mode-hook #'add-node-modules-path))
-(eval-after-load 'yaml-mode '(add-hook 'yaml-mode-hook #'add-node-modules-path))
 
 (provide 'init-javascript)
