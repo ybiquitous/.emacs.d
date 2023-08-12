65e8dd0472cdc0b0ca2ef5c84921f9ec8ca2f842
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-08-12 22:59:53 +0900

    Refactor `init-ruby` settings

--- inits/init-ruby.el
+++ inits/init-ruby.el
@@ -1,46 +1,9 @@
-(require 'ruby-mode)
-(setq ruby-insert-encoding-magic-comment nil)
-
-(use-package enh-ruby-mode
-  :interpreter "ruby"
-  :mode ("\\.rb\\'"
-          "\\.gemspec\\'"
-          "\\.irbrc\\'"
-          "\\.jb\\'"
-          "\\.jbuilder\\'"
-          "\\.rake\\'"
-          "\\.ru\\'"
-          "Capfile\\'"
-          "Gemfile\\'"
-          "Guardfile\\'"
-          "Rakefile\\'"
-          "Steepfile\\'"
-          "Vagrantfile\\'")
+(use-package ruby-mode
   :custom
-  (enh-ruby-add-encoding-comment-on-save nil)
-  (enh-ruby-deep-indent-paren nil)
-  (enh-ruby-bounce-deep-indent t))
+  (ruby-insert-encoding-magic-comment nil))
 
 (use-package ruby-electric
-  :hook ((ruby-mode enh-ruby-mode) . ruby-electric-mode))
-
-;; https://www.emacswiki.org/emacs/HideShow
-(add-to-list 'hs-special-modes-alist
-  '(ruby-mode
-     "class\\|module\\|def\\|if\\|unless\\|case\\|while\\|until\\|for\\|begin\\|do"
-     "end"
-     "#"
-     ruby-move-to-block
-     nil))
-(add-to-list 'hs-special-modes-alist
-  '(enh-ruby-mode
-     "class\\|module\\|def\\|if\\|unless\\|case\\|while\\|until\\|for\\|begin\\|do"
-     "end"
-     "#"
-     ruby-move-to-block
-     nil))
-(add-hook 'ruby-mode-hook #'hs-minor-mode)
-(add-hook 'enh-ruby-mode-hook #'hs-minor-mode)
+  :hook (ruby-base-mode . ruby-electric-mode))
 
 (use-package rbs-mode)
 
