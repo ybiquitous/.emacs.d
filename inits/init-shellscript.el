bfc50ff0f795db4360b9b0fb31c582e9cd234066
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-08-12 15:35:48 +0900

    Refactor `init-shellscript` config with `use-package`

--- inits/init-shellscript.el
+++ inits/init-shellscript.el
@@ -1,7 +1,7 @@
-(require 'sh-script)
-(setq sh-basic-offset tab-width)
-
-;; https://github.com/direnv/direnv
-(add-to-list 'auto-mode-alist '("\\.envrc\\'" . sh-mode))
+(use-package sh-script
+  ;; https://github.com/direnv/direnv
+  :mode ("\\.envrc\\'" . sh-mode)
+  :custom
+  (sh-basic-offset tab-width))
 
 (provide 'init-shellscript)
