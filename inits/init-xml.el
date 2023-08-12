2d9ebe4d6ac4810ca08163ef566786b4fda14d8c
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-08-12 22:51:17 +0900

    Refactor `init-xml` settings with `use-package`

--- inits/init-xml.el
+++ inits/init-xml.el
@@ -1,6 +1,8 @@
-(require 'nxml-mode)
-(setq nxml-auto-insert-xml-declaration-flag t)
-(setq nxml-default-buffer-file-coding-system (quote utf-8))
-(setq nxml-slash-auto-complete-flag t)
+(use-package nxml-mode
+  :ensure nil
+  :custom
+  (nxml-auto-insert-xml-declaration-flag t)
+  (nxml-default-buffer-file-coding-system (quote utf-8))
+  (nxml-slash-auto-complete-flag t))
 
 (provide 'init-xml)
