adf1d14bb89b94207c4a7454d6488e7e180bd941
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-08-12 22:09:11 +0900

    Refactor `wdired` settings with `use-package`

--- inits/init-dired.el
+++ inits/init-dired.el
@@ -1,6 +1,8 @@
-(require 'wdired)
-(setq wdired-allow-to-change-permissions t)
-(define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)
+(use-package wdired
+  :custom
+  (wdired-allow-to-change-permissions t)
+  :bind (:map dired-mode-map
+         ("e" . wdired-change-to-wdired-mode)))
 
 (use-package dired-sidebar
   :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
