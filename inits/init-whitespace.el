2c97ea45fdd04c8b915a2cc7b8f87f3c501c789b
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-08-12 22:49:07 +0900

    Refactor `whitespace` settings with `use-package`

--- inits/init-whitespace.el
+++ inits/init-whitespace.el
@@ -1,10 +1,11 @@
-(require 'whitespace)
-(setq whitespace-action '(auto-cleanup))
-(setq whitespace-display-mappings '((space-mark ?\u3000 [?\u25a1])
-                                     (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t])))
-(setq whitespace-space-regexp "\\(\\u3000+\\)")
-(setq whitespace-line-column 120)
-(setq whitespace-style '(face spaces trailing lines-tail space-mark tab-mark))
-(global-whitespace-mode t)
+(use-package whitespace
+  :custom
+  (whitespace-action '(auto-cleanup))
+  (whitespace-display-mappings '((space-mark ?\u3000 [?\u25a1])
+                                 (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t])))
+  (whitespace-space-regexp "\\(\\u3000+\\)")
+  (whitespace-line-column 120)
+  (whitespace-style '(face spaces trailing lines-tail space-mark tab-mark))
+  (global-whitespace-mode t))
 
 (provide 'init-whitespace)
