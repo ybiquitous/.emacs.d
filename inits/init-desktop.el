9984cb4c76c2851c9bba0863e73cc7c0280efb78
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-08-12 22:03:17 +0900

    Refactor `desktop` settings with `use-package`

--- inits/init-desktop.el
+++ inits/init-desktop.el
@@ -1,7 +1,8 @@
-(require 'desktop)
-(setq desktop-files-not-to-save nil)
-(setq desktop-globals-to-save (quote (extended-command-history)))
-(setq desktop-restore-frames nil)
-(desktop-save-mode t)
+(use-package desktop
+  :custom
+  (desktop-files-not-to-save nil)
+  (desktop-globals-to-save (quote (extended-command-history)))
+  (desktop-restore-frames nil)
+  (desktop-save-mode t))
 
 (provide 'init-desktop)
