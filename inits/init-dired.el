2c2f0fda4e8598756b68b71daed27d2214c1568b
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-08-15 23:07:41 +0900

    Fix `dired-mode-map` not working

--- inits/init-dired.el
+++ inits/init-dired.el
@@ -1,8 +1,9 @@
 (use-package wdired
   :custom
   (wdired-allow-to-change-permissions t)
-  :bind (:map dired-mode-map
-         ("e" . wdired-change-to-wdired-mode)))
+  :config
+  (bind-keys :map dired-mode-map
+    ("e" . wdired-change-to-wdired-mode)))
 
 (use-package dired-sidebar
   :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
