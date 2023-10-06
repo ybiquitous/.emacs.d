8f2461880148aeae25c5544e2b3889daa2f09ff8
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-10-06 11:42:14 +0900

    Try fixing CI failure

--- inits/init-copilot.el
+++ inits/init-copilot.el
@@ -4,17 +4,15 @@
   (message "Installing or updating copilot.el...")
   (if (file-directory-p package-dir)
     (shell-command (format "git -C '%s' pull --quiet" package-dir))
-    (shell-command (format "git clone --quiet '%s' '%s'" repo-url package-dir))))
-
-(use-package copilot
-  :load-path "git-packages/copilot.el"
-  :ensure nil
-  ;; :hook
-  ;; (emacs-lisp-mode . copilot-mode)
-  :bind (:map copilot-completion-map
-          ("<tab>" . 'copilot-accept-completion)
-          ("TAB" . 'copilot-accept-completion)
-          ("C-TAB" . 'copilot-accept-completion-by-word)
-          ("C-<tab>" . 'copilot-accept-completion-by-word)))
+    (shell-command (format "git clone --quiet '%s' '%s'" repo-url package-dir)))
+  (use-package copilot
+    :load-path "git-packages/copilot.el/"
+    ;; :hook
+    ;; (emacs-lisp-mode . copilot-mode)
+    :bind (:map copilot-completion-map
+            ("<tab>" . 'copilot-accept-completion)
+            ("TAB" . 'copilot-accept-completion)
+            ("C-TAB" . 'copilot-accept-completion-by-word)
+            ("C-<tab>" . 'copilot-accept-completion-by-word))))
 
 (provide 'init-copilot)
