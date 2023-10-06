e0c7d6e4eeb1875bfbfeafb982e6a03925720886
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-10-06 12:06:47 +0900

    Fix CI failure due to copilot.el (#14)
    For example: https://github.com/ybiquitous/.emacs.d/actions/runs/6426811522/job/17451531259

--- inits/init-copilot.el
+++ inits/init-copilot.el
@@ -1,13 +1,14 @@
 ;; Install from GitHub
 (let* ((repo-url "https://github.com/zerolfx/copilot.el")
         (package-dir (expand-file-name "git-packages/copilot.el" user-emacs-directory)))
-  (message "Installing or updating copilot.el...")
+  (message "Installing or updating 'copilot.el'...")
   (if (file-directory-p package-dir)
-    (shell-command (format "git -C '%s' pull --quiet" package-dir))
-    (shell-command (format "git clone --quiet '%s' '%s'" repo-url package-dir))))
+    (shell-command (format "git -C '%s' pull" package-dir))
+    (shell-command (format "git clone '%s' '%s'" repo-url package-dir))))
 
 (use-package copilot
-  :load-path "git-packages/copilot.el"
+  :after (:all editorconfig s)
+  :load-path "git-packages/copilot.el/"
   ;; :hook
   ;; (emacs-lisp-mode . copilot-mode)
   :bind (:map copilot-completion-map
