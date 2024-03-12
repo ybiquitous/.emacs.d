afda0a5940328737be0fada33d43abb2f8332bb5
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-03-12 23:08:45 +0900

    Override `js-find-symbol` key-binding for `lsp-mode`

--- inits/init-javascript.el
+++ inits/init-javascript.el
@@ -6,6 +6,12 @@
   ;; Node.js supports `.mjs' and `.cjs': https://nodejs.org/api/packages.html
   (add-to-list 'auto-mode-alist '("\\.[cm]js\\'" . js-mode)))
 
+;; HACK: `js-find-symbol' does not work with `lsp-mode', so this workaround resets the keymap.
+;; See https://github.com/emacs-lsp/lsp-mode/issues/2770#issuecomment-816450453
+(with-eval-after-load 'js
+  (define-key js-mode-map (kbd "M-.") nil)
+  (define-key js-ts-mode-map (kbd "M-.") nil))
+
 (use-package add-node-modules-path
   :custom
   (add-node-modules-path-debug t)
