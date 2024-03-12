f3a941a27048a7817322743151d13a71c55e6acd
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-03-12 22:35:25 +0900

    Use built-in Rubocop support in lsp-mode
    Ref https://emacs-lsp.github.io/lsp-mode/page/lsp-rubocop/

--- inits/init-lsp.el
+++ inits/init-lsp.el
@@ -2,14 +2,7 @@
   :commands lsp
   :hook (lsp-mode . lsp-enable-which-key-integration)
   :custom
-  (lsp-disabled-clients '(deno-ls)))
-
-;; https://emacs-lsp.github.io/lsp-mode/page/adding-new-language/
-(with-eval-after-load 'lsp-mode
-  (lsp-register-client
-    (make-lsp-client :new-connection (lsp-stdio-connection '("bundle" "exec" "rubocop" "--lsp"))
-                     :activation-fn (lsp-activate-on "ruby")
-                     :add-on? t
-                     :server-id 'rubocop-ls)))
+  (lsp-disabled-clients '(deno-ls))
+  (lsp-rubocop-use-bundler t))
 
 (provide 'init-lsp)
