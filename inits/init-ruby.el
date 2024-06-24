1f34c3ccac63d428e763bd9c1ba5006219b93802
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-06-25 01:01:28 +0900

    Fix `my/add-bundler-exec-path` error when `Gemfile` is not found
    ```sh-session
    $ ruby -r bundler -e 'print Bundler.bundle_path.join(%{bin})'
    /Users/masafumi.koba/.rbenv/versions/3.3.3/lib/ruby/site_ruby/3.3.0/bundler.rb:328:in `rescue in root': Could not locate Gemfile or .bundle/ directory (Bundler::GemfileNotFound)
    	from /Users/masafumi.koba/.rbenv/versions/3.3.3/lib/ruby/site_ruby/3.3.0/bundler.rb:324:in `root'
    	from /Users/masafumi.koba/.rbenv/versions/3.3.3/lib/ruby/site_ruby/3.3.0/bundler.rb:101:in `bundle_path'
    	from -e:1:in `<main>'
    ```

--- inits/init-ruby.el
+++ inits/init-ruby.el
@@ -1,10 +1,11 @@
 (defun my/add-bundler-exec-path ()
   "Add the path to gems installed by Bundler to the `exec-path`."
   (interactive)
-  (let* ((command "ruby -r bundler -e 'print Bundler.bundle_path.join(%{bin})'")
-         (bundler-gems-bin-path (shell-command-to-string command)))
-    (message "Added to `exec-path`: %s" bundler-gems-bin-path)
-	  (setq-local exec-path (cons bundler-gems-bin-path exec-path))))
+  (let* ((command "ruby -r bundler -e 'begin print Bundler.bundle_path.join(%{bin}) ; rescue Bundler::BundlerError ; end'")
+          (bundler-gems-bin-path (shell-command-to-string command)))
+    (unless (string-empty-p bundler-gems-bin-path)
+      (message "Added to `exec-path`: %s" bundler-gems-bin-path)
+	    (setq-local exec-path (cons bundler-gems-bin-path exec-path)))))
 (add-hook 'ruby-base-mode-hook #'my/add-bundler-exec-path)
 
 (use-package ruby-mode
