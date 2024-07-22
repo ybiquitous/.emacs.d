a9181365acb484cbfe5be799aa163871757d6aa9
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-07-22 22:37:10 +0900

    Refactor `js-find-symbol` remap to `xref-find-definitions`

--- inits/init-javascript.el
+++ inits/init-javascript.el
@@ -1,16 +1,12 @@
 (use-package js
+  ;; See https://github.com/joaotavora/eglot/discussions/1348
+  :bind ([remap js-find-symbol] . xref-find-definitions)
   :custom
   (js-indent-level tab-width)
   (js-switch-indent-offset tab-width)
   :init
   ;; Node.js supports `.mjs' and `.cjs': https://nodejs.org/api/packages.html
   (add-to-list 'auto-mode-alist '("\\.[cm]js\\'" . js-mode)))
-
-;; HACK: `js-find-symbol' does not work with `lsp-mode', so this workaround resets the keymap.
-;; See https://github.com/emacs-lsp/lsp-mode/issues/2770#issuecomment-816450453
-(with-eval-after-load 'js
-  (define-key js-mode-map (kbd "M-.") nil)
-  (define-key js-ts-mode-map (kbd "M-.") nil))
 
 ;; HACK: Prevent the error on CI: `Cannot open load file: No such file or directory, f'
 ;; See https://github.com/ybiquitous/.emacs.d/actions/runs/8982929204/job/24671576283#step:5:474
