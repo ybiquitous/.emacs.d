42b54ba73fbb40472e9f90e3965f73c4a080e6e1
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-08-13 22:10:50 +0900

    Support `.cts` and `.mts` files for `typescript-ts-mode`

--- inits/init-typescript.el
+++ inits/init-typescript.el
@@ -1,4 +1,4 @@
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.[cm]?ts\\'" . typescript-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
 
 (provide 'init-typescript)
