5fd2135f2d432cde26384f4ea6005cca5ecae9a9
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-09-15 23:27:48 +0900

    Improve output of `my/cheatsheet`

--- inits/init-key-bindings.el
+++ inits/init-key-bindings.el
@@ -6,29 +6,47 @@
 
 Key        Function
 -------    -------------------------------------------
+<Help>
 C-c k b    `describe-personal-keybindings'
 C-? b      `describe-bindings'
 C-? f      `describe-function'
 C-? k      `describe-key'
+
+<Edit>
 C-q        `quoted-insert' (e.g. `C-q TAB')
 C-x 8      `insert-char' (e.g. emoji `😇')
 C-x C-+    `text-scale-adjust' (↑)
 C-x C--    `text-scale-adjust' (↓)
 C-x C-0    `text-scale-adjust' (reset)
 C-x SPC    `rectangle-mark-mode'
+C-x C-q    `view-mode'
+C-c l      `toggle-truncate-lines'
+C-c a r    `align-regexp'
+
+<VC>
 C-c C-g    `vc-git-grep'
 C-x v =    `vc-diff'
 C-x v g    `vc-annotate' (git blame)
 C-x v l    `vc-print-log'
 C-x v u    `vc-revert'
+
+<Shell>
 M-!        `shell-command'
 M-&        `async-shell-command'
 C-u M-!    `shell-command' (insert to buffer)
 C-u M-|    `shell-command-on-region' (insert to buffer)
+
+<iSearch>
 C-M-s      `isearch-forward-regexp'
 M-s w      `isearch-forward-word'
+
+<Misc>
+C-c M-r    `restart-emacs'
+C-c C-r    `revert-buffer-quick'
+C-c v      `browse-url-at-point'
 M-o        `other-frame'
-")))
+")
+    (switch-to-buffer "*Cheatsheet*")))
 
 (require 'bind-key)
 (bind-keys*
