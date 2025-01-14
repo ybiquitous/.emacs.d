d271df772ee2d225a80cd7691d698a1a604a0f89
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-01-14 11:08:05 +0900

    Set `user-emacs-directory`

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ set -e
 ${EMACS:=emacs} --no-window-system --batch --eval '
 (progn
   (setq debug-on-error t)
+  (setq user-emacs-directory default-directory)
   (load-file (expand-file-name "init.el" default-directory))
   (run-hooks (quote after-init-hook)))
 '
