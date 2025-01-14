37511431e04c488cbab6b9762ac44fa947121288
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-01-14 11:05:10 +0900

    Enable `debug-on-error` on test

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 
-${EMACS:=emacs} --no-window-system --batch \
-                --eval '(let ((user-emacs-directory default-directory))
-                          (load-file (expand-file-name "init.el" user-emacs-directory))
-                          (run-hooks (quote after-init-hook)))'
+${EMACS:=emacs} --no-window-system --batch --eval '
+(progn
+  (setq debug-on-error t)
+  (load-file (expand-file-name "init.el" default-directory))
+  (run-hooks (quote after-init-hook)))
+'
