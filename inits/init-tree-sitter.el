e71adc9eb718de367050756968f68370b896186b
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-02-17 16:54:38 +0900

    Introduce `treesit-fold` and `treesit-fold-indicators`
    Ref https://github.com/emacs-tree-sitter/treesit-fold

--- inits/init-tree-sitter.el
+++ inits/init-tree-sitter.el
@@ -10,4 +10,14 @@
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
 
+(my/download-github-package "https://github.com/emacs-tree-sitter/treesit-fold")
+(use-package treesit-fold
+  :load-path (lambda () "git-packages/treesit-fold")
+  :delight " TS-Fold"
+  :hook ((ruby-ts-mode yaml-ts-mode) . treesit-fold-mode))
+
+(use-package treesit-fold-indicators
+  :load-path (lambda () "git-packages/treesit-fold")
+  :hook ((ruby-ts-mode yaml-ts-mode) . treesit-fold-indicators-mode))
+
 (provide 'init-tree-sitter)
