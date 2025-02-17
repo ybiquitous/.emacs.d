718300120ed82185214e91b4446bdc15e919cf36
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-02-17 16:56:31 +0900

    Simplify `hideshow` settings to avoid conflicts with `treesit-fold`

--- inits/init-hideshow.el
+++ inits/init-hideshow.el
@@ -1,28 +1,8 @@
 ;; https://www.emacswiki.org/emacs/HideShow
-(use-package hideshow
-  :bind ("C-c C-:" . hs-toggle-hiding)
-  :hook (prog-mode . hs-minor-mode)
-  :config
-  (add-to-list 'hs-special-modes-alist
-    '(ruby-mode
-       "class\\|module\\|def\\|if\\|unless\\|case\\|while\\|until\\|for\\|begin\\|do"
-       "end"
-       "#"
-       ruby-move-to-block
-       nil))
-  (add-to-list 'hs-special-modes-alist
-    '(ruby-ts-mode
-       "class\\|module\\|def\\|if\\|unless\\|case\\|while\\|until\\|for\\|begin\\|do"
-       "end"
-       "#"
-       ruby-move-to-block
-       nil)))
+(use-package hideshow)
 
 (my/download-github-package "https://github.com/sheijk/hideshowvis")
 (use-package hideshowvis
-  :load-path (lambda () "git-packages/hideshowvis")
-  :hook (prog-mode . hideshowvis-enable)
-  :config
-  (hideshowvis-symbols))
+  :load-path (lambda () "git-packages/hideshowvis"))
 
 (provide 'init-hideshow)
