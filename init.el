87fecf74f7e50f1d5e9f8c615dce39f862f90882
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-02-27 00:07:27 +0900

    Remove unintentional change

--- init.el
+++ init.el
@@ -140,16 +140,3 @@
 (setq custom-file "~/.emacs-env.el")
 (when (file-exists-p custom-file)
   (load custom-file))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-vc-selected-packages
-    '((treesit-fold :url "https://github.com/emacs-tree-sitter/treesit-fold"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
