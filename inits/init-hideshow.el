1e12854dbe1149e6f55094205e0d86f19601744d
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-02-27 00:25:01 +0900

    Enable `hideshowvis` on `emacs-lisp-mode` (#24)

--- inits/init-hideshow.el
+++ inits/init-hideshow.el
@@ -2,6 +2,7 @@
 (use-package hideshow)
 
 (use-package hideshowvis
-  :vc (:url "https://github.com/sheijk/hideshowvis"))
+  :vc (:url "https://github.com/sheijk/hideshowvis")
+  :hook (emacs-lisp-mode . hideshowvis-minor-mode))
 
 (provide 'init-hideshow)
