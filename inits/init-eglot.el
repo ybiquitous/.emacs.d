525a0f8b01d59ad2a3b5a59c8e53431bc2ca59d5
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-04-30 12:30:49 +0900

    Remove rarely used Steep from eglot
    Note: `ruby-lsp` is included by default.

--- inits/init-eglot.el
+++ inits/init-eglot.el
@@ -1,9 +1,6 @@
 (use-package eglot
   :config
   (add-to-list 'eglot-server-programs
-    `((ruby-mode ruby-ts-mode) . ,(eglot-alternatives
-                                    '(("steep" "langserver" "--verbose")
-                                      ("ruby-lsp"))))
     `((yaml-mode yaml-ts-mode) . ,(eglot-alternatives
                                     '(("actions-languageserver" "--stdio"))))))
 
