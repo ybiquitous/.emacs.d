e7fbc0fd498a068d7b8f878e3b3ab3ef06faa572
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-06-04 15:05:01 +0900

    Auto-run `eglot-upgrade-eglot` when `eglot` is loaded

--- inits/init-eglot.el
+++ inits/init-eglot.el
@@ -2,6 +2,7 @@
   :custom
   (eglot-autoshutdown t)
   :config
+  (eglot-upgrade-eglot)
   (add-to-list 'eglot-server-programs
     `((yaml-mode yaml-ts-mode) . ,(eglot-alternatives
                                     '(("actions-languageserver" "--stdio"))))))
