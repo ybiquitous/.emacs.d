bb81f7603fd0b360734976e86aa84d0860b6605d
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-06-07 11:13:29 +0900

    Configure `mermaid-mode`

--- inits/init-mermaid.el
+++ inits/init-mermaid.el
@@ -1,3 +1,7 @@
-(use-package mermaid-mode)
+(use-package mermaid-mode
+  :custom
+  (mermaid-flags "--scale=2")
+  (mermaid-output-format ".png")
+  (mermaid-tmp-dir (concat (expand-file-name "tmp" "~") "/")))
 
 (provide 'init-mermaid)
