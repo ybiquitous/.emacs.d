7db0789f7202ce59f51c8a7ae6758add2f022b21
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-08-15 12:12:33 +0900

    Add `tsc --lsp` support for `eglot`

--- inits/init-eglot.el
+++ inits/init-eglot.el
@@ -5,6 +5,12 @@
   (eglot-upgrade-eglot)
   (add-to-list 'eglot-server-programs
     `((yaml-mode yaml-ts-mode) . ,(eglot-alternatives
-                                    '(("actions-languageserver" "--stdio"))))))
+                                    '(("actions-languageserver" "--stdio")))))
+
+  ;; TypeScript 7.0 (tsc) has the LSP built-in support.
+  (add-to-list 'eglot-server-programs
+    `((js-mode js-ts-mode typescript-mode typescript-ts-mode tsx-ts-mode) . ,(eglot-alternatives
+                                                                               '(("typescript-language-server" "--stdio")
+                                                                                 ("tsc" "--lsp" "--stdio"))))))
 
 (provide 'init-eglot)
