497e49cf3af13dfbcb7121718e138fc7038f0cb5
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-08-17 17:45:35 +0900

    Uninstall unused `copilot` package

--- inits/init-copilot.el
+++
@@ -1,9 +0,0 @@
-(use-package copilot
-  :after (:all editorconfig s)
-  :bind (:map copilot-completion-map
-          ("<tab>" . 'copilot-accept-completion)
-          ("TAB" . 'copilot-accept-completion)
-          ("C-TAB" . 'copilot-accept-completion-by-word)
-          ("C-<tab>" . 'copilot-accept-completion-by-word)))
-
-(provide 'init-copilot)
