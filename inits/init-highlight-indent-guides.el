a1b9cbeb4c6fe9a0df7945ee087084af219d5ad2
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-08-17 19:08:58 +0900

    Replace `highlight-indent-guides` with `indent-bars`
    `indent-bars` is faster and has more features.
    
    Ref: https://github.com/jdtsmith/indent-bars

--- inits/init-highlight-indent-guides.el
+++
@@ -1,7 +0,0 @@
-(use-package highlight-indent-guides
-  :hook ((prog-mode yaml-ts-mode) . highlight-indent-guides-mode)
-  :custom
-  (highlight-indent-guides-auto-character-face-perc 80)
-  (highlight-indent-guides-method 'character))
-
-(provide 'init-highlight-indent-guides)
---
+++ inits/init-indent-bars.el
@@ -0,0 +1,6 @@
+(use-package indent-bars
+  :hook ((prog-mode yaml-ts-mode) . indent-bars-mode)
+  :custom
+  (indent-bars-treesit-support t))
+
+(provide 'init-indent-bars)
