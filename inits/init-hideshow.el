ac9a15e3b12460623c1f8ce515c8fc5d2afb8b65
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-08-17 23:25:24 +0900

    Add TODO comment to drop `hideshowvis` package in Emacs 31
    The next major version, Emacs 31, plans to implement the built-in feature that
    is the same as `hideshowvis`. The third-party package will no longer be needed.

--- inits/init-hideshow.el
+++ inits/init-hideshow.el
@@ -1,6 +1,7 @@
 ;; https://www.emacswiki.org/emacs/HideShow
 (use-package hideshow)
 
+;; TODO: A similar feature is going to be implemented in Emacs 31. Drop `hideshowvis' after the upgrade.
 (use-package hideshowvis
   :hook (emacs-lisp-mode . hideshowvis-minor-mode))
 
