de10618b66195a5f1a0730438dba65950ab900ec
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-09-02 13:43:10 +0900

    Remove unneeded `hideshowvis` package for published Emacs 31.1

--- inits/init-hideshow.el
+++ inits/init-hideshow.el
@@ -1,8 +1,8 @@
 ;; https://www.emacswiki.org/emacs/HideShow
-(use-package hideshow)
-
-;; TODO: A similar feature is going to be implemented in Emacs 31. Drop `hideshowvis' after the upgrade.
-(use-package hideshowvis
-  :hook (emacs-lisp-mode . hideshowvis-minor-mode))
+(use-package hideshow
+  :hook (emacs-lisp-mode . hs-minor-mode)
+  :custom
+  (hs-display-lines-hidden t)
+  (hs-show-indicators t))
 
 (provide 'init-hideshow)
