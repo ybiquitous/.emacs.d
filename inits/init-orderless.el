5601404d7c40d6eec6b8867a0fbab27e2237ae6c
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-09-02 13:56:25 +0900

    Fix workaround for `orderless` package with Emacs 31.1

--- inits/init-orderless.el
+++ inits/init-orderless.el
@@ -2,8 +2,8 @@
 (use-package orderless
   :custom
   (completion-styles '(orderless basic))
-  ;; TODO: Drop `basic' from `completion-category-overrides' and add `(completion-pcm-leading-wildcard t)' after upgrading to Emacs 31.
-  (completion-category-overrides '((file (styles basic partial-completion))
-                                    (eglot-capf (styles orderless)))))
+  (completion-category-overrides '((file (styles partial-completion))
+                                   (eglot-capf (styles orderless))))
+  (completion-pcm-leading-wildcard t))
 
 (provide 'init-orderless)
