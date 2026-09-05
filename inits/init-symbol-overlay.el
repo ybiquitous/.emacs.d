69a510ab62023cc9b344cd239835b967cfac72f2
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-09-05 17:16:38 +0900

    Enable `symbol-overlay-mode` for automatic symbol highlighting

--- inits/init-symbol-overlay.el
+++ inits/init-symbol-overlay.el
@@ -1,6 +1,11 @@
 ;; -*- lexical-binding: t -*-
 (use-package symbol-overlay
+  :hook (prog-mode . symbol-overlay-mode)
   :bind (("M-i" . symbol-overlay-put)
-         ("M-I" . symbol-overlay-remove-all)))
+         ("M-I" . symbol-overlay-remove-all))
+  :custom
+  (symbol-overlay-idle-time 1.0)
+  :custom-face
+  (symbol-overlay-default-face ((t (:background "#33372a")))))
 
 (provide 'init-symbol-overlay)
